631a6bbab00f1f94dd1b851f5bf2fbb944a93a20
Ben Boeckel <MathStuf@gmail.com>
2009-11-08 16:02:24 -0500

    Set the position on the map

--- player.py
+++ player.py
@@ -26,6 +26,8 @@ class Player(DirectObject):
     def _load_models(self):
         self._model = Actor("player")
         self._model.reparentTo(render)
+        self._model.setX(0)
+        self._model.setY(0)
         self._model.setScale(.001)
 
     def _load_lights(self):
